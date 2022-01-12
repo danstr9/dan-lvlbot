f9a3385b9171aba8a6188964f0ae1301d25c7db8
mbuffagni <matteo.buffagni@webranking.it>
2022-01-12 02:02:56 +0100

    sp deamon but it's kinda fucked up, need to test it in prod

--- vars.py
+++ vars.py
@@ -1,6 +1,6 @@
 ######################## GLOBAL VARIABLES #########################
 BINANCE = False # Variable to indicate which exchange to use: True for BINANCE, False for FTX
-active_pairs = ['ATOM-PERP']
+active_pairs = ['FTT-PERP', 'UNI-PERP']
 MULTI_TF = True
 TF_LIST = ['5m', '15m', '1h', '4h']
 TF = '1h'
