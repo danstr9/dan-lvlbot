8e2dd8b8ae3ece3c8494778d1bc837b978d55210
mbuffagni <matteo.buffagni@webranking.it>
2022-01-12 02:06:14 +0100

    decrease balance per pair

--- vars.py
+++ vars.py
@@ -1,25 +1,25 @@
 ######################## GLOBAL VARIABLES #########################
-BINANCE = False # Variable to indicate which exchange to use: True for BINANCE, False for FTX
+BINANCE = False  # Variable to indicate which exchange to use: True for BINANCE, False for FTX
 active_pairs = ['FTT-PERP', 'UNI-PERP']
 MULTI_TF = True
 TF_LIST = ['5m', '15m', '1h', '4h']
 TF = '1h'
-LEVELS_PER_TF = 3 # Number of levels per Time Frame to include in the list of DCAs
-DAYS_BACK = 90 # Number of days to look back in time for initial candles data
+LEVELS_PER_TF = 3  # Number of levels per Time Frame to include in the list of DCAs
+DAYS_BACK = 90  # Number of days to look back in time for initial candles data
 TRADE_ON = True
 ###################################################################
 
 # Balance variables
-MAX_BAL_PER_COIN = 25 # Maximum percentage of balance to use per asset/coin
+MAX_BAL_PER_COIN = 20  # Maximum percentage of balance to use per asset/coin
 LVRG = 20
 
 # Take Profit variables
-TPGRID_MIN_DIST = 1 # Percentage to use for the closest order in the TP grid
+TPGRID_MIN_DIST = 1  # Percentage to use for the closest order in the TP grid
 TPGRID_MAX_DIST = 4  # Percentage to use for the farthest order in the TP grid
-TP_ORDERS_NUMBER = 6 # Number of orders for the TP grid
-DCA_FACTOR_MULT = 1.75 # Factor to multiply each DCA entry in the grid (it will multiply the amount of previous DCA by this factor)
-ASSYMMETRIC_TP = False # False for equal sized orders, False for descending size TP orders 
-MIN_LEVEL_DISTANCE = 1.2 # Variable to indicate what % will be the minimum valid distance between levels found
+TP_ORDERS_NUMBER = 6  # Number of orders for the TP grid
+DCA_FACTOR_MULT = 1.75  # Factor to multiply each DCA entry in the grid (it will multiply the amount of previous DCA by this factor)
+ASSYMMETRIC_TP = False  # False for equal sized orders, False for descending size TP orders
+MIN_LEVEL_DISTANCE = 1.2  # Variable to indicate what % will be the minimum valid distance between levels found
 MAX_DCA_NUMBER = 9
 SP_TRIGGER_LIST = [1.5, 2.5, 3.2]
 SP_MINIMUM_LIST = [0.8, 1.5, 2.5]
@@ -27,7 +27,7 @@ LEVEL_COLUMNS = ['side', 'price', 'hit', 'time']
 
 # Candles calculation
 if 'm' in TF:
-    TF_NUM = int(TF.split('m')[0]) # Used for FTX candles
+    TF_NUM = int(TF.split('m')[0])  # Used for FTX candles
     NUM_CANDLES = int(DAYS_BACK * 24 * 60 / TF_NUM)
 elif 'h' in TF:
     TF_NUM = int(TF.split('h')[0]) * 60
